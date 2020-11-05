381f16663a927dabc3c490171f44ba7d16a414dc
Isaac Gros <isaac.gros888@gmail.com>
2020-11-05 23:56:22 +0100

    [BACK/FEAT] Added Webpack Encore

---
+++ server/webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // Webpack config
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .enableSassLoader()
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // Assets
+    .addEntry('app', './assets/app.js')
+    .addStyleEntry('styles', './assets/styles/app.scss')
+;
+
+module.exports = Encore.getWebpackConfig();
