f63ac6b32c445b90da1226477d29fd64006e25a1
Isaac Gros <isaac.gros888@gmail.com>
2020-11-07 00:14:03 +0100

    [BACK/FEAT] Added dashboard first styles

--- server/src/Controller/DashboardController.php
+++ server/src/Controller/DashboardController.php
@@ -14,8 +14,50 @@ class DashboardController extends AbstractController
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // List of quick actions
+        $actions = [
+            [
+                'icon' => 'home',
+                'text' => 'Éditer l\'accueil',
+                'path' => '#'
+            ],
+            [
+                'icon' => 'file',
+                'text' => 'Nouvelle page',
+                'path' => '#'
+            ],
+            [
+                'icon' => 'tasks',
+                'text' => 'Nouveau projet',
+                'path' => '#'
+            ],
+            [
+                'icon' => 'music',
+                'text' => 'Modifier la playlist',
+                'path' => '#'
+            ]
+        ];
+
+        // List of content overviews
+        $overviews = [
+            [
+                'title' => 'Pages',
+                'items' => []
+            ],
+            [
+                'title' => 'Projets',
+                'items' => []
+            ],
+            [
+                'title' => 'Playlist actuelle',
+                'items' => []
+            ]
+        ];
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'actions' => $actions,
+            'overviews' => $overviews
         ]);
     }
 }
