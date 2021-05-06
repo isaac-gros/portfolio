395995cb6cd6297fceb7606bcbb9fe2fae3c87cc
Isaac Gros <isaac.gros888@gmail.com>
2021-05-06 23:24:03 +0200

    [FIX] Added Bootstrap & Froala

--- server/assets/scripts/editor.js
+++ server/assets/scripts/editor.js
@@ -1,6 +1,9 @@
 import FroalaEditor from 'froala-editor';
 import 'froala-editor/js/plugins/link.min.js'
 
+/**
+ * WYSIWYG Froala Editor
+ */
 new FroalaEditor('#editor', {
     toolbarButtons: ['bold', 'insertLink'],
     heightMin: 140,
